9a5ab40b4e085ee3c770f382a550a9cbb2641b09
Anand Bagmar <abagmar@gmail.com>
2020-10-16 18:11:39 +0530

    added few tests that launch on android app

---
+++ src/test/java/com/jiomeet/e2e/RunCukes.java
@@ -0,0 +1,15 @@
+package com.jiomeet.e2e;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
+
+public class RunCukes extends AbstractTestNGCucumberTests {
+  @Override
+  @DataProvider(parallel = true)
+  public Object[][] scenarios() {
+    System.out.printf("ThreadID: %d: in overridden scenarios%n", Thread.currentThread().getId());
+    Object[][] scenarios = super.scenarios();
+    System.out.println(scenarios);
+    return scenarios;
+  }
+}
